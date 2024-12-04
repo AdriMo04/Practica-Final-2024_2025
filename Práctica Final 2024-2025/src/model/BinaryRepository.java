f367e473969682e6b11a62a00fdce3300ee23d21
Adrián Moreira <id00860110@usal.es>
2024-12-04 13:44:35 +0100

    Continuar con la clase BinaryRepository

--- Práctica Final 2024-2025/src/model/BinaryRepository.java
+++ Práctica Final 2024-2025/src/model/BinaryRepository.java
@@ -1,5 +1,30 @@
 package model;
 
-public class BinaryRepository {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class BinaryRepository implements IRepository {
+    private static final String FILE_PATH = System.getProperty("user.home") + "/tasks.bin";
+    private List<Task> tasks = new ArrayList<>();
+
+    @Override
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    @Override
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    @Override
+    public void modifyTask(Task task) {
+        // ¿?
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+        return tasks;
+    }
 }
