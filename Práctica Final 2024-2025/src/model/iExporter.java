d1be01da1faa66b3836f6d1923bfe47a5e05da74
Adrián Moreira <id00860110@usal.es>
2024-12-10 00:26:03 +0100

    Excepciones

--- Práctica Final 2024-2025/src/model/iExporter.java
+++ Práctica Final 2024-2025/src/model/iExporter.java
@@ -1,5 +1,8 @@
 package model;
 
-public interface IExporter {
+import java.util.List;
 
+public interface IExporter {
+    void export(List<Task> tasks, String filePath);
+    List<Task> importTasks(String filePath);
 }
