f5a4958b97ddc335ba3f61f403a02b49f5b77e5d
Adrián Moreira <id00860110@usal.es>
2024-12-09 17:50:13 +0100

    Clase BaseView

--- Práctica Final 2024-2025/src/view/BaseView.java
+++ Práctica Final 2024-2025/src/view/BaseView.java
@@ -1,5 +1,23 @@
 package view;
 
-public abstract class BaseView {
+import controller.Controller;
 
+public abstract class BaseView {
+    protected Controller controller;
+
+    // Constructor:
+    public BaseView (Controller controller) {
+        this.controller = controller;
+    }
+
+    // Setter
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    // Métodos
+    public abstract void init(); // Inicia la vista y desencadena la lógica asociada
+    public abstract void showMessage(String Message); // Muestra mensajes genéricos al usuario
+    public abstract void showErrorMessage(String errorMessage); // Muestra mensajes de error
+    public abstract void end(); // Finaliza la vista ordenadamente
 }
