6b2bf90eb7f74378718f5fb413c4c30ae5565a75
Adrián Moreira <id00860110@usal.es>
2024-12-09 17:31:13 +0100

    Clase BinaryRepository

--- Práctica Final 2024-2025/src/model/BinaryRepository.java
+++ Práctica Final 2024-2025/src/model/BinaryRepository.java
@@ -11,20 +11,43 @@ public class BinaryRepository implements IRepository {
     @Override
     public void addTask(Task task) {
         tasks.add(task);
+        save();
     }
 
     @Override
     public void removeTask(Task task) {
         tasks.remove(task);
+        save();
     }
 
     @Override
     public void modifyTask(Task task) {
-        // ¿?
+        int index = tasks.indexOf(task);
+        if (index != -1) {
+            tasks.set(index, task);
+            save();
+        }
     }
 
     @Override
     public List<Task> getAllTasks() {
+        load();
         return tasks;
+    }
+
+    public void save() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH))) {
+            oos.writeObject(tasks);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void load() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_PATH))) {
+            tasks = (List<Task>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            tasks = new ArrayList<>();
+        }
     }
 }
