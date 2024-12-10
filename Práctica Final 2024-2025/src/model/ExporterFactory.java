990711d3347327b4ff82bb69111cb71752f3c019
Adrián Moreira <id00860110@usal.es>
2024-12-10 18:49:28 +0100

    Clase ExporterFactory

--- Práctica Final 2024-2025/src/model/ExporterFactory.java
+++ Práctica Final 2024-2025/src/model/ExporterFactory.java
@@ -1,5 +1,16 @@
 package model;
 
-public class ExporterFactory {
+import model.exceptions.ExporterException;
 
+public class ExporterFactory {
+    public static IExporter getExporter(String tipo) throws ExporterException {
+        switch (tipo.toLowerCase()) {
+            case "json":
+                return (IExporter) new JSONExporter();
+            case "csv":
+                return (IExporter) new CSVExporter();
+            default: 
+                throw new ExporterException("Tipo de exportador no soportado: " + tipo);
+        }
+    }
 }
