40735590766d9ebd80cf2fae1a58938dcacea2b6
Adrián Moreira <id00860110@usal.es>
2024-12-11 17:12:00 +0100

    Clase JSONExporter

--- Práctica Final 2024-2025/src/model/JSONExporter.java
+++ Práctica Final 2024-2025/src/model/JSONExporter.java
@@ -1,21 +1,39 @@
 package model;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.*;
+import java.lang.reflect.Type;
 import java.util.List;
 
 import model.exceptions.ExporterException;
 
 public class JSONExporter implements IExporter{
+    private final Gson gson;
+
+    public JSONExporter() {
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+    }
 
     @Override
     public void export(List<Task> tasks, String filePath) throws ExporterException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'export'");
+        try (FileWriter writer = new FileWriter(filePath)) {
+            gson.toJson(tasks, writer);
+        } catch (IOException e) {
+            throw new ExporterException("Error exportando tareas a JSON: " + e.getMessage(), e);
+        }
     }
 
     @Override
     public List<Task> importTasks(String filePath) throws ExporterException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'importTasks'");
+        try (FileReader reader = new FileReader(filePath)) {
+            Type listType = new TypeToken<List<Task>>() {}.getType();
+            return gson.fromJson(reader, listType);
+        } catch (IOException e) {
+            throw new ExporterException("Error importando tareas desde JSON: " + e.getMessage(), e);
+        }
     }
 
 }
